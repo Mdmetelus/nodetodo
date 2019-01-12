0a5427893b0c41f68b37cbd559c811f5dd8ca65a
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-12 15:47:45 -0500

    get and post request

--- routes/api/todo.js
+++ routes/api/todo.js
@@ -8,4 +8,22 @@ router.get('/', (req, res) => {
     })
 });
 
+router.post('/', (req,res) => {
+    const sendthis = req.body;
+
+    console.log(req.body);
+
+    db.insert(sendthis).returning('*').into('todo')
+        .then( (date) =>{ res.send(data) })
+        .catch(res.send({ error: 'Error' }))
+    
+    //INSERT INTO tablename(column1, column2) VALUES(column_value, column2_value);
+
+    //SELECT * FROM table WHERE id = insert_row;
+    
+
+    res.send(hello);
+    // db.insert().then().catch()
+})
+
 module.exports = router;
