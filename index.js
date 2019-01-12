ee9314027c51ab9d6eb697bd0e82db2808eac15e
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-12 14:55:14 -0500

    "updates"

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const apiRoute =require('./routes/api');
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use('/api', apiRoute );
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Body parser:
+app.use(bodyParser.json());
+// without extended you would only have a simple object, 
+app.use(bodyParser.urlencoded({extended: true}));
+//you would not be able to hav accessth nested objects.
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 app.get('', (req, res) => {});
 
-const port = process.env.PORT || 5678;
+const port = process.env.PORT || 3456;
 app.listen(port, () => console.log(`The Server is listening on port ${port}.`));
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const todoRoute = require('./todo');
 
-router.use('todo', todoRouter);
+router.use('/todo', todoRoute);
+//localhost:3000/api/todo
+
+
 
 module.exports= router;
